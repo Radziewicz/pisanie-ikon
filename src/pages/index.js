4c833a0e53f505e49d409b618eb59d0bbc33de0d
Radziewicz <radziewicz87@wp.pl>
2019-02-07 12:29:18 +0100

    hero initial styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default () => (
-  <Layout> 
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
+  <Layout>
+      <div className="wrapper">
+        <div className="startowa">
+          <a href="" ><div className="square"></div></a>
+          <a href="" ><div className="square"></div></a>
+        </div>
+      </div>
   </Layout>
-)
+);
--- src/pages/test.js
+++ src/pages/test.js
@@ -15,6 +15,7 @@ export default class IndexPage extends React.Component {
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
             </div>
+            <div className="blog">
             {posts
               .map(({ node: post }) => (
                 <div
@@ -39,6 +40,7 @@ export default class IndexPage extends React.Component {
                   </p>
                 </div>
               ))}
+              </div>
           </div>
         </section>
       </Layout>
